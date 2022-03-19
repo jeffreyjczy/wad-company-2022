6567e22dbcd77b262cc3305d7e53c2f00fc23261
JeffreyJCZY <jeffreyzhiyeechong@gmail.com>
2022-03-19 16:51:21 +0700

    update

--- app.js
+++ app.js
@@ -28,10 +28,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.static(path.join(__dirname, "public", "react-quotation")));
-app.get("/react-quotation/*", function (req, res) {
-
+app.get("", function (req, res) {
   res.sendFile(path.join(__dirname, "public", "react-quotation", "index.html"));
-
 });
 
 // Plug routers
