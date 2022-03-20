4b8bf092b91186c0a4291757c2445da7786993c6
JeffreyJCZY <jeffreyzhiyeechong@gmail.com>
2022-03-20 15:30:32 +0700

    database Update

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productsRouter = require('./routes/products');
 var customerRouter = require('./routes/customers');
+var quotationRouter = require('./routes/quotations');
 
 var app = express();
 
@@ -37,6 +38,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
 app.use('/customers',customerRouter);
+app.use('/quotations',quotationRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
